3ebd4befdbf61643f206dccb935f0e463aac583e
George Talusan <george.talusan@gmail.com>
2024-09-04 15:05:46 -0400

    detab

--- src/eufy-robovac-config.js
+++ src/eufy-robovac-config.js
@@ -1,48 +1,48 @@
 const { RoboVac } = require('@george.talusan/eufy-robovac-js');
 
 module.exports = (RED) => {
-	class EufyRobovacConfigNode {
-	  constructor (config) {
-		  RED.nodes.createNode(this, config);
+  class EufyRobovacConfigNode {
+    constructor (config) {
+      RED.nodes.createNode(this, config);
 
-			this.connected = false;
-			this.reconnect = true;
+      this.connected = false;
+      this.reconnect = true;
 
-			const { ip, deviceId, localKey } = config;
-			this.robovac = new RoboVac({ ip, deviceId, localKey });
-			this.robovac.on('tuya.connected', async () => {
-				console.warn('connected');
+      const { ip, deviceId, localKey } = config;
+      this.robovac = new RoboVac({ ip, deviceId, localKey });
+      this.robovac.on('tuya.connected', async () => {
+        console.warn('connected');
 
-				this.connected = true;
-			});
+        this.connected = true;
+      });
 
-			this.robovac.on('tuya.disconnected', async () => {
-				this.connected = false;
-				if (!this.reconnect) {
-					return;
-				}
+      this.robovac.on('tuya.disconnected', async () => {
+        this.connected = false;
+        if (!this.reconnect) {
+          return;
+        }
 
-				console.warn('reconnecting..');
+        console.warn('reconnecting..');
 
-				const id = setInterval(async () => {
-					try {
-						await this.robovac.connect();
-						clearInterval(id);
-					} catch (e) {
-						console.error(e);
-					}
-				}, 2000);
-			});
+        const id = setInterval(async () => {
+          try {
+            await this.robovac.connect();
+            clearInterval(id);
+          } catch (e) {
+            console.error(e);
+          }
+        }, 2000);
+      });
 
-			this.robovac.initialize();
+      this.robovac.initialize();
 
-			this.on('close', () => {
-				console.warn('close');
+      this.on('close', () => {
+        console.warn('close');
 
-				this.reconnect = false;
-				this.robovac.disconnect();
-			});
-		}
-	}
-	RED.nodes.registerType('eufy-robovac-config', EufyRobovacConfigNode);
+        this.reconnect = false;
+        this.robovac.disconnect();
+      });
+    }
+  }
+  RED.nodes.registerType('eufy-robovac-config', EufyRobovacConfigNode);
 };
--- src/eufy-robovac-output.js
+++ src/eufy-robovac-output.js
@@ -1,42 +1,42 @@
 module.exports = (RED) => {
-	class EufyRobovacOutputNode {
-	  constructor (config) {
-		  RED.nodes.createNode(this, config);
+  class EufyRobovacOutputNode {
+    constructor (config) {
+      RED.nodes.createNode(this, config);
 
-			const node = this;
-			const configNode = RED.nodes.getNode(config.config);
-			if (!configNode) {
-				node.status({ fill: 'red', shape: 'dot', text: 'Not configured' });
+      const node = this;
+      const configNode = RED.nodes.getNode(config.config);
+      if (!configNode) {
+        node.status({ fill: 'red', shape: 'dot', text: 'Not configured' });
         node.error('eufy robovac config missing');
-				return;
-			}
+        return;
+      }
 
-			const robovac = configNode.robovac;
-			const events = config.output.split(',');
-			events.forEach(event => {
-				if (!event) {
-					return;
-				}
-				robovac.on(event, (data) => {
-					const msg = {
-						payload: data
-					};
-					node.send(msg);
-				});
-			});
+      const robovac = configNode.robovac;
+      const events = config.output.split(',');
+      events.forEach(event => {
+        if (!event) {
+          return;
+        }
+        robovac.on(event, (data) => {
+          const msg = {
+            payload: data
+          };
+          node.send(msg);
+        });
+      });
 
-			node.status({ fill: 'green', shape: 'dot', text: 'Connected' });
-	  }
-	}
+      node.status({ fill: 'green', shape: 'dot', text: 'Connected' });
+    }
+  }
 
-	RED.nodes.registerType('out', EufyRobovacOutputNode,
-		{
-			defaults: {
-				config: {
-					value: '',
-					type: 'eufy-robovac-config'
-				}
-			}
-		}
-	);
+  RED.nodes.registerType('out', EufyRobovacOutputNode,
+    {
+      defaults: {
+        config: {
+          value: '',
+          type: 'eufy-robovac-config'
+        }
+      }
+    }
+  );
 };
